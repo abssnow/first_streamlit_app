2d8f8ed30d90d883db10f3227bff806893de9d03
abssnow <107780661+abssnow@users.noreply.github.com>
2022-06-19 15:05:41 +1200

    Update streamlit_app.py
    correct header misspelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.headier('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 and Blueberry oatmeal')
 streamlit.text('Kale Spinach and Rocket muesli')
