db2b5c8e4d0cded868d7995bd4ad2720854f8385
abssnow <107780661+abssnow@users.noreply.github.com>
2022-06-19 16:01:34 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
 streamlit.text('🥗 Kale Spinach and Rocket muesli')
 streamlit.text('🐔 Hard Boiled free range egg')
 streamlit.text('🍞 Avocado toast')
-
+streamlit.text('🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
